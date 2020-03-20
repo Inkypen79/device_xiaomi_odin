c7dd38983694cdfa79e260f61a7a9faba099b01f
Arian <arian.kulmer@web.de>
2020-03-20 16:11:24 +0100

    odin: Add extract scripts

---
+++ setup-makefiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+
+DEVICE=odin
+VENDOR=xiaomi
+INITIAL_COPYRIGHT_YEAR=2021
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+LINEAGE_ROOT="${MY_DIR}/../../.."
+
+HELPER="${LINEAGE_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper for common
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
+
+# Copyright headers and guards
+write_headers "${DEVICE}"
+
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
