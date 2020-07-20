21d9434d804aa37428df55c01ab7ec592844383d
Michael Bestas <mkbestas@lineageos.org>
2020-07-20 23:16:26 +0300

    odin: parts: Make use of standard pickup sensor
     * Use it for the pickup feature instead of the custom xiaomi sensor
    
    Change-Id: Ie2572fec6d4307ce0b555f6ee4dbf92c9cf7bd87

--- parts/src/org/lineageos/settings/doze/PickupSensor.java
+++ parts/src/org/lineageos/settings/doze/PickupSensor.java
@@ -19,72 +19,54 @@ package org.lineageos.settings.doze;
 
 import android.content.Context;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.SystemClock;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.util.Log;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class PickupSensor implements SensorEventListener {
+public class PickupSensor {
+
     private static final boolean DEBUG = false;
     private static final String TAG = "PickupSensor";
-
-    private static final int MIN_PULSE_INTERVAL_MS = 2500;
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context mContext;
     private ExecutorService mExecutorService;
 
-    private long mEntryTimestamp;
-
     public PickupSensor(Context context) {
         mContext = context;
         mSensorManager = mContext.getSystemService(SensorManager.class);
-        mSensor = DozeUtils.getSensor(mSensorManager, "xiaomi.sensor.pickup");
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PICK_UP_GESTURE);
         mExecutorService = Executors.newSingleThreadExecutor();
     }
 
     private Future<?> submit(Runnable runnable) { return mExecutorService.submit(runnable); }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (DEBUG)
-            Log.d(TAG, "Got sensor event: " + event.values[0]);
-
-        long delta = SystemClock.elapsedRealtime() - mEntryTimestamp;
-        if (delta < MIN_PULSE_INTERVAL_MS) {
-            return;
-        }
-
-        mEntryTimestamp = SystemClock.elapsedRealtime();
-
-        if (event.values[0] == 1) {
-            DozeUtils.wakeOrLaunchDozePulse(mContext);
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        /* Empty */
-    }
-
     protected void enable() {
         if (DEBUG)
             Log.d(TAG, "Enabling");
         submit(() -> {
-            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
-            mEntryTimestamp = SystemClock.elapsedRealtime();
+            mSensorManager.requestTriggerSensor(mPickupListener, mSensor);
         });
     }
 
     protected void disable() {
         if (DEBUG)
             Log.d(TAG, "Disabling");
-        submit(() -> { mSensorManager.unregisterListener(this, mSensor); });
+        submit(() -> { mSensorManager.cancelTriggerSensor(mPickupListener, mSensor); });
     }
+
+    private TriggerEventListener mPickupListener = new TriggerEventListener() {
+        @Override
+        public void onTrigger(TriggerEvent event) {
+            if (DEBUG) Log.d(TAG, "Triggered");
+            DozeUtils.wakeOrLaunchDozePulse(mContext);
+            enable();
+        }
+    };
 }
