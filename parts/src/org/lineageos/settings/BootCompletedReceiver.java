2b4d68a06dba3821d3f724e76690dd01283a3ac1
Arian <arian.kulmer@web.de>
2020-10-14 23:12:48 +0200

    odin: parts: clang-format everything
    Change-Id: Iae92b7197a58b4353dafa7e24d67ebb5f096f2b5

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,13 +25,13 @@ import android.util.Log;
 import org.lineageos.settings.doze.DozeUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
-
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
     }
 }
