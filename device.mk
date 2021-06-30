e91fed2bcba603230326f4d2ef25867004e894e1
Albert I <kras@raphielgang.org>
2021-06-30 19:06:09 +0800

    odin: Set up display color compositions
    * Switch to "Adaptive" mode and Display P3 color by default
    
    * Use default color mode for Natural and Boosted
    
    * Enable QDCM combined mode and disable factory mode
    
    * Drop LiveDisplay's color mode support as it conflicts with AOSP display
      color adjustment
    
    * Rest of configuration is kanged off Pixel's overlay
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: Ib26033fe00a6cc1e8c7d8f93da417516e31a52e3

--- device.mk
+++ device.mk
@@ -214,6 +214,8 @@ $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,fal
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.sdm
 
+$(call soong_config_set_bool,livedisplay_sdm,enable_dm,false)
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor \
