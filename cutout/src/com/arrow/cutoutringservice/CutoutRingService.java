7cb62fed3a279a1fb23984624abc5bf1e0bf29ad
zaidkhan0997 <kzaid0997@gmail.com>
2021-10-19 19:18:42 +0530

    odin: cutout: increase camera cutout RING_SIZE
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- cutout/src/com/arrow/cutoutringservice/CutoutRingService.java
+++ cutout/src/com/arrow/cutoutringservice/CutoutRingService.java
@@ -51,7 +51,7 @@ public class CutoutRingService extends BroadcastReceiver {
     private static final String TAG = "CutoutRingServiceOdin";
     private static final String FRONT_CAMERA_ID = "1";
 
-    private static final int RING_SIZE = 64;
+    private static final int RING_SIZE = 84;
     private static final int X_OFFSET = 0;
     private static final int X_OFFSET_HORIZONTAL = 20;
     private static final int Y_OFFSET = 20;
