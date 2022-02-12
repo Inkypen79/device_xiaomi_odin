2e9e3aee9575c987ef94290eb0340989ab9ae518
Nicholas Magill <nmagill1979@gmail.com>
2022-02-12 22:45:05 +0000

    odin: Build vendor variant of libtextclassifier_hash

--- device.mk
+++ device.mk
@@ -346,6 +346,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Neural Net
+PRODUCT_PACKAGES += \
+    libtextclassifier_hash.vendor \
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
