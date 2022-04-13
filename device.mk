58bb62d60dea4edf14f44038043bb6abfc9c889f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    odin: Move to common IFAAService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2

--- device.mk
+++ device.mk
@@ -262,10 +262,7 @@ PRODUCT_PACKAGES += \
 
 # IFAA manager
 PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+    IFAAService
 
 # IPACM
 PRODUCT_PACKAGES += \
