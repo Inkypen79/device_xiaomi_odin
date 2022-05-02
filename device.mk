2832b8191d02efbd3ae275c7e52508d2dd22d1ca
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    odin: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- device.mk
+++ device.mk
@@ -325,6 +325,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor:64
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
