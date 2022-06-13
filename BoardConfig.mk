2d684c506b31f215e8eac13250535344ece84daf
Nicholas Magill <nmagill1979@gmail.com>
2022-06-13 19:40:11 +0100

    odin: Include firmware images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -258,3 +258,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 -include vendor/xiaomi/odin/BoardConfigVendor.mk
+-include vendor/xiaomi/odin-firmware/BoardConfigVendor.mk
