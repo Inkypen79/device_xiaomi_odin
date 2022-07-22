f69f39f61c4545f0fe1d2a125f22e0d032e5bc33
Nicholas Magill <nmagill1979@gmail.com>
2022-07-22 19:51:46 +0100

    odin: Compile kernel with Clang 15.0.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,11 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-if [ ! -d "${CLANGDIR}/clang-r450784e" ]
+if [ ! -d "${CLANGDIR}/clang-r458507" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 14.0.7${NC}"
-    mkdir ${CLANGDIR}/clang-r450784e
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r450784e.tar.gz -P ${CLANGDIR}
-    tar -C "${CLANGDIR}"/clang-r450784e/ -zxf "${CLANGDIR}"/clang-r450784e.tar.gz
-    rm ${CLANGDIR}/clang-r450784e.tar.gz
+    echo -e "${GREEN}Downloading clang 15.0.1${NC}"
+    mkdir ${CLANGDIR}/clang-r458507
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r458507.tar.gz -P ${CLANGDIR}
+    tar -C "${CLANGDIR}"/clang-r458507/ -zxf "${CLANGDIR}"/clang-r458507.tar.gz
+    rm ${CLANGDIR}/clang-r458507.tar.gz
 fi
