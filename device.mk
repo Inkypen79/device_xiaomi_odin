a59b6978c62cdf1f145c4425283fe8dc79f9389c
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    odin: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     libdrm
 
 # Exclude sensor from InputManager
