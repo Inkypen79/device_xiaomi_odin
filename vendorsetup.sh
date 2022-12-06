72b0d98a5b5b01bdf83a73424c94c4ec0823716b
Nicholas Magill <nmagill1979@gmail.com>
2022-12-06 11:15:46 +0000

    odin: Compile kernel with Clang 16.0.1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-if [ ! -d "${CLANGDIR}/clang-r468909b" ]
+CLANGVER=clang-r475365
+if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 15.0.3${NC}"
-    mkdir ${CLANGDIR}/clang-r468909b
-    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/clang-r468909b.tar.gz -P ${CLANGDIR}
-    tar -C "${CLANGDIR}"/clang-r468909b/ -zxf "${CLANGDIR}"/clang-r468909b.tar.gz
-    rm ${CLANGDIR}/clang-r468909b.tar.gz
+    echo -e "${GREEN}Downloading clang 16.0.1${NC}"
+    git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}"
 fi
