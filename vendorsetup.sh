3fcf13e2c827e42e97aa31b2f2948c969afd0f26
Nicholas Magill <nmagill1979@gmail.com>
2023-01-17 22:33:35 +0000

    odin: Compile kernel with Clang 16.0.2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-CLANGVER=clang-r475365
+CLANGVER=clang-r475365b
 if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 16.0.1${NC}"
+    echo -e "${GREEN}Downloading clang 16.0.2${NC}"
     git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}"
 fi
