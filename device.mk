ea0b287c98a453869bf120b6f4fac621621dc2dd
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    odin: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- device.mk
+++ device.mk
@@ -289,8 +289,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
