b00a9f9fffca5256ca62e0bf1c8ad6bc4db44acb
Nicholas Magill <nmagill1979@gmail.com>
2023-03-12 11:12:27 +0000

    odin: Import task_profiles.json from vendor

--- device.mk
+++ device.mk
@@ -347,6 +347,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3 \
     vendor.qti.hardware.perf@2.3.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
