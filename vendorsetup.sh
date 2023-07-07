56248d109689d9c35ad5ba42365f62ae37b10b68
Nicholas Magill <nmagill1979@gmail.com>
2023-07-07 23:49:02 +0100

    odin: Compile kernel with Clang 17.0.2
    * The issue with hard-coded paths for AR and LD
      has been resolved, drop unneccesary build flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-CLANGVER=clang-r487747
+CLANGVER=clang-r487747c
 if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 17.0${NC}"
-    git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}"
+    echo -e "${GREEN}Downloading clang 17.0.2${NC}"
+    git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
