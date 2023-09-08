36f071b9be2516bb9423db6970d73cd28461f5ba
Nicholas Magill <nmagill1979@gmail.com>
2023-09-08 23:16:30 +0100

    odin: Import hardware/xiaomi namespace

--- device.mk
+++ device.mk
@@ -413,7 +413,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/nxp
+    hardware/nxp \
+    hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
