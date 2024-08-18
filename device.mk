ab007253b5bd1e51c58df1d42818b2d6f20f3b63
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-08-18 23:59:08 +0200

    odin: Move to Xiaomi IR AIDL
    Change-Id: I46094bcbfaba252b5d98ea8211b927fdaa079e2d

--- device.mk
+++ device.mk
@@ -118,8 +118,7 @@ PRODUCT_COPY_FILES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
