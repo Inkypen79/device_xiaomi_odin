43b269a01d4fd63a88ab0a00c5fbc20f33e0088a
Hadad <hadad@linuxmail.org>
2024-09-26 11:45:31 +0000

    odin: Include Xiaomi common FCM.
    Change-Id: I11a95944bc80d476750afcde9d5b4506344ce5bb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,6 +117,7 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # Init
