b2a141c01434f046bc33e955ccc0adc6f4fb5b52
Nicholas Magill <nmagill1979@gmail.com>
2024-11-24 01:32:55 +0000

    odin: Build DeviceAsWebcam

--- device.mk
+++ device.mk
@@ -146,6 +146,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.credentials.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.credentials.xml \
 
+# DeviceAsWebcam
+PRODUCT_PACKAGES += \
+    DeviceAsWebcam
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
