63a00be131cdf50e79933ae3aef5a55fc52d1bd7
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    odin: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # Powershare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 
