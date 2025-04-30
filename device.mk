f159f03a94fbd1409e9e5ebf9d64916618c479e0
Bruno Martins <bgcngm@gmail.com>
2025-04-30 21:12:33 +0100

    odin: Set camera override format from reserved via soong config
    Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

--- device.mk
+++ device.mk
@@ -98,6 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti.recovery
 
 # Camera
+$(call soong_config_set,camera,override_format_from_reserved,true)
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 PRODUCT_PACKAGES += \
