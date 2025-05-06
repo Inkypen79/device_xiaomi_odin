645614870fae3646ff04536aeadd9dcc5f5efd94
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    odin: switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa

--- device.mk
+++ device.mk
@@ -118,7 +118,7 @@ PRODUCT_COPY_FILES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
