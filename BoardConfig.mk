fb907a729f432abf4d58b158f1bb3d1f61443d02
basamaryan <basam.aryan@gmail.com>
2025-07-08 15:51:28 -0400

    odin: Override kernel BPF version
    Change-Id: I3c581f3120b6258b3b45020c5ba12f6bf5b0ee27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,7 @@ TARGET_BOARD_PLATFORM := lahaina
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
