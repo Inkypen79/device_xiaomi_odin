e06fc0a8d07af4d6dd65918f5013708bdd790d17
Nicholas Magill <nmagill1979@gmail.com>
2025-12-24 11:28:29 +0000

    odin: Drop UWB HAL hack
    * Device name fixed in kernel now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,3 @@ then
     echo -e "${GREEN}Downloading clang 21.0.0${NC}"
     git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
-
-cd $PWD/hardware/nxp/uwb
-git fetch https://android.googlesource.com/platform/hardware/nxp/uwb 0d0dd875c90ebc33e9a76c8397a7218742b51730 --quiet
-git checkout FETCH_HEAD --quiet
-croot
