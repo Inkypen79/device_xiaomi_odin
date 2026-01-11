f14e62359fdc34e4f98b8854e335040c1e14e929
Nicholas Magill <nmagill1979@gmail.com>
2026-01-11 20:46:36 +0000

    fixup! odin: Add firmware extraction support

--- extract-files.py
+++ extract-files.py
@@ -167,6 +167,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
