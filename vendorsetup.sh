b66fe7443cc730f2452b19edb61c96df7cf7ab03
Nicholas Magill <nmagill1979@gmail.com>
2026-02-16 16:09:08 +0000

    odin: Append odin/uwb to libchrome visibility

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,13 @@ then
     echo -e "${GREEN}Downloading clang 21.0.0${NC}"
     git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
+
+SEARCH_LINE="//device/google/cheets2/camera/v3"
+INSERT_LINE="//device/xiaomi/odin/uwb"
+cd $PWD/external/libchrome
+if grep -qF "$INSERT_LINE" Android.bp; then
+    croot
+else
+    sed -i "\|\"$SEARCH_LINE\"|a \        \"$INSERT_LINE\"," Android.bp
+    croot
+fi
